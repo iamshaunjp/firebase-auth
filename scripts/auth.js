c026f7bc273cf97ff9b6086ba0ce9b886a0dce35
Shaun <shaun@thenetninja.co.uk>
2018-12-02 22:13:52 +0000

    lesson-12

--- scripts/auth.js
+++ scripts/auth.js
@@ -9,7 +9,24 @@ auth.onAuthStateChanged(user => {
     setupUI();
     setupGuides([]);
   }
-})
+});
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('guides').add({
+    title: createForm.title.value,
+    content: createForm.content.value
+  }).then(() => {
+    // close the create modal & reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message);
+  });
+});
 
 // signup
 const signupForm = document.querySelector('#signup-form');
