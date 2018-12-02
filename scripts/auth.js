2e62b8f5c32c5c732714accaafdfbe02e9afb78e
Shaun <shaun@thenetninja.co.uk>
2018-12-02 13:40:28 +0000

    lesson-5

---
+++ scripts/auth.js
@@ -0,0 +1,19 @@
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    // console.log(cred.user);
+  }).then(() => {
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
