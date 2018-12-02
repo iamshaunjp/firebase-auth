3ac74b4597736f23c6f2588a14d2c576342d9179
Shaun <shaun@thenetninja.co.uk>
2018-12-02 18:23:21 +0000

    lesson-7

--- scripts/auth.js
+++ scripts/auth.js
@@ -24,3 +24,23 @@ logout.addEventListener('click', (e) => {
     console.log('user signed out');
   })
 });
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+
+});
