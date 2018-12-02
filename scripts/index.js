09547a6b1f06eecd4dc35deb2218b322afd356cf
Shaun <shaun@thenetninja.co.uk>
2018-12-02 22:27:05 +0000

    lesson-14

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,21 @@
 const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
 
 const setupUI = (user) => {
   if (user) {
+    // account info
+    const html = `
+      <div>Logged in as ${user.email}</div>
+    `;
+    accountDetails.innerHTML = html;
     // toggle user UI elements
     loggedInLinks.forEach(item => item.style.display = 'block');
     loggedOutLinks.forEach(item => item.style.display = 'none');
   } else {
+    // clear account info
+    accountDetails.innerHTML = '';
     // toggle user elements
     loggedInLinks.forEach(item => item.style.display = 'none');
     loggedOutLinks.forEach(item => item.style.display = 'block');
