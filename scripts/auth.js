60df1fae8a66261beda618cae052dc239362cbfb
Shaun <shaun@thenetninja.co.uk>
2018-12-03 12:47:49 +0000

    lesson-20

--- scripts/auth.js
+++ scripts/auth.js
@@ -13,9 +13,12 @@ adminForm.addEventListener('submit', (e) => {
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
+    user.getIdTokenResult().then(idTokenResult => {
+      user.admin = idTokenResult.claims.admin;
+      setupUI(user);
+    });
     db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
-      setupUI(user);
     }, err => console.log(err.message));
   } else {
     setupUI();
--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,19 @@ const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 const accountDetails = document.querySelector('.account-details');
+const adminItems = document.querySelectorAll('.admin');
 
 const setupUI = (user) => {
   if (user) {
+    if (user.admin) {
+      adminItems.forEach(item => item.style.display = 'block');
+    }
     // account info
     db.collection('users').doc(user.uid).get().then(doc => {
       const html = `
         <div>Logged in as ${user.email}</div>
         <div>${doc.data().bio}</div>
+        <div class="pink-text">${user.admin ? 'Admin' : ''}</div>
       `;
       accountDetails.innerHTML = html;
     });
@@ -21,6 +26,7 @@ const setupUI = (user) => {
     // clear account info
     accountDetails.innerHTML = '';
     // toggle user elements
+    adminItems.forEach(item => item.style.display = 'none');
     loggedInLinks.forEach(item => item.style.display = 'none');
     loggedOutLinks.forEach(item => item.style.display = 'block');
   }
