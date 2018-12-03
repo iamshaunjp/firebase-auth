4098467b5665409c16352cdc4279f25b971e9e6c
Shaun <shaun@thenetninja.co.uk>
2018-12-03 12:20:16 +0000

    lesson-19

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,15 @@
+// add admin cloud function
+const adminForm = document.querySelector('.admin-actions');
+adminForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const adminEmail = document.querySelector('#admin-email').value;
+  const addAdminRole = functions.httpsCallable('addAdminRole');
+  addAdminRole({ email: adminEmail }).then(result => {
+    console.log(result);
+  });
+});
+
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
