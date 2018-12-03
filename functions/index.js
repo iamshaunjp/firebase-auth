24f2288f37589e18dbece19130414b69a361ffaf
Shaun <shaun@thenetninja.co.uk>
2018-12-03 12:03:46 +0000

    lesson-18

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  // get user and add admin custom claim
+  return admin.auth().getUserByEmail(data.email).then(user => {
+    return admin.auth().setCustomUserClaims(user.uid, {
+      admin: true
+    })
+  }).then(() => {
+    return {
+      message: `Success! ${data.email} has been made an admin.`
+    }
+  }).catch(err => {
+    return err;
+  });
+});
