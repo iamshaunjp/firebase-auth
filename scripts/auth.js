8312460388c77ed3e0df8ef59f0627641bd01f50
Shaun <shaun@thenetninja.co.uk>
2018-12-03 13:54:00 +0000

    lesson-23

--- scripts/auth.js
+++ scripts/auth.js
@@ -62,6 +62,9 @@ signupForm.addEventListener('submit', (e) => {
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
+    signupForm.querySelector('.error').innerHTML = ''
+  }).catch(err => {
+    signupForm.querySelector('.error').innerHTML = err.message;
   });
 });
 
@@ -87,6 +90,9 @@ loginForm.addEventListener('submit', (e) => {
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
     loginForm.reset();
+    loginForm.querySelector('.error').innerHTML = '';
+  }).catch(err => {
+    loginForm.querySelector('.error').innerHTML = err.message;
   });
 
 });
