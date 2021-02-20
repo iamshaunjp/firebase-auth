56e8cc9a362e7250da7f1c8e56480cb858c5b3e2
Awoyomi Mayowa <mayowaawoyomi@gmail.com>
2021-02-20 18:33:42 +0100

    corrected the code in lesson 10

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,56 +1,119 @@
-// listen for auth status changes
+//access to the  elements class with logged in and logged out
+const loggedInLinks = document.querySelectorAll('.logged-in')
+const loggedOutLinks = document.querySelectorAll('.logged-out')
+
+//to toggle depending on the situation
+
+const setUpUi = (user) => {
+    if (user) {
+        loggedInLinks.forEach(item => {
+            item.style.display = 'block'
+        });
+
+        loggedOutLinks.forEach(item => {
+            item.hidden = true
+        });
+
+    } else {
+        loggedInLinks.forEach(item => {
+            item.hidden = true
+        });
+
+        loggedOutLinks.forEach(item => {
+            item.style.display = 'block'
+        });
+    }
+}
+
+
+// listen for auth status change
 auth.onAuthStateChanged(user => {
-  if (user) {
-    console.log('user logged in: ', user);
-    db.collection('guides').get().then(snapshot => {
-      setupGuides(snapshot.docs);
-    });
-  } else {
-    console.log('user logged out');
-    setupGuides([]);
-  }
+    if (user) {
+        //to get and addd guides with firestore
+        db.collection('guides').get().then((snapshot) => {
+            console.log(snapshot.docs)
+            renderGuide(snapshot.docs)
+            setUpUi(user)
+        })
+    } else {
+        renderGuide([])
+        setUpUi()
+    }
+
+    (user) ? console.log('user is loggedin', user) : console.log('user is logged out')
+
+
 })
 
-// signup
-const signupForm = document.querySelector('#signup-form');
+
+
+
+
+
+// the signup form
+const signupForm = document.querySelector('#signup-form')
+
+// access to the input value when i submit the form
 signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
+    e.preventDefault();
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
 
-  // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    // close the signup modal & reset form
-    const modal = document.querySelector('#modal-signup');
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
-  });
-});
+    //values inputed
+    console.log(email, password)
 
-// logout
-const logout = document.querySelector('#logout');
+    //sign up the user into the firebase auth
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred)
+        const modal = document.querySelector('#modal-signup')
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    })
+
+})
+
+
+//log out
 logout.addEventListener('click', (e) => {
-  e.preventDefault();
-  auth.signOut();
-});
+    e.preventDefault();
+    auth.signOut()
+})
 
-// login
-const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
+//log in
+const login = document.querySelector('#login-form')
+login.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = login["login-email"].value
+    const password = login["login-password"].value
 
-  // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    // close the signup modal & reset form
-    const modal = document.querySelector('#modal-login');
-    M.Modal.getInstance(modal).close();
-    loginForm.reset();
-  });
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred)
+        const modal = document.querySelector('#modal-login')
+        M.Modal.getInstance(modal).close();
+        login.reset();
+    })
 
-});
+})
+
+
+//create new guides
+
+let guideForm = document.querySelector('#create-form')
+guideForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    db.collection('guides').add({
+        title: guideForm['title'].value,
+        content: guideForm['content'].value
+    }).then(() => {
+        const modal = document.querySelector('#modal-create')
+        M.Modal.getInstance(modal).close();
+        guideForm.reset()
+    })
+
+
+
+
+})
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,31 @@
-// DOM elements
-const guideList = document.querySelector('.guides');
+//access to the ul element
+const ulElem = document.querySelector('.guides')
 
-// setup guides
-const setupGuides = (data) => {
+function renderGuide(data) {
+  if(data.length){
+    let html =''
 
-  if (data.length) {
-    let html = '';
-    data.forEach(doc => {
-      const guide = doc.data();
-      const li = `
-        <li>
-          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
-          <div class="collapsible-body white"> ${guide.content} </div>
-        </li>
-      `;
-      html += li;
-    });
-    guideList.innerHTML = html
-  } else {
-    guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>';
+    data.forEach(element => {
+        console.log(element.data())
+        let guide = element.data()
+
+    const li = `
+    <li>
+    <div class="collapsible-header grey lighten-4">${guide.title}</div>
+    <div class="collapsible-body white">${guide.content}</div>
+    </li>
+    `
+    html += li
+
+    })
+
+    ulElem.innerHTML = html
+  }else{
+    ulElem.innerHTML =`<h5 class="center-align">You have logged out, <b>log in </b>to view guides</h5>`
   }
-  
+    
 
-};
+}
 
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
