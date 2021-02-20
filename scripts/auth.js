7e0bd16596785f72d43dd68768f242a905335182
Awoyomi Mayowa <mayowaawoyomi@gmail.com>
2021-02-20 10:50:23 +0100

    update from lesson 1-10

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,8 +1,30 @@
-// access to the form through the id
-const signupForm = document.getElementById('signup-form');
+// listen for auth status change
+auth.onAuthStateChanged(user => {
+    if(user){
+        //to get and addd guides with firestore
+        db.collection('guides').get().then((snapshot) => {
+            console.log(snapshot.docs)
+            renderGuide(snapshot.docs)
+        })
+    }else{
+        renderGuide([])
+    }
+
+    (user) ? console.log('user is loggedin', user) : console.log('user is logged out')
+
+
+})
+
+
+
+
+
+
+// the signup form
+const signupForm = document.querySelector('#signup-form')
 
 // access to the input value when i submit the form
-signupForm.addEventListener('submit',(e)=>{
+signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = signupForm['signup-email'].value;
     const password = signupForm['signup-password'].value;
@@ -10,22 +32,37 @@ signupForm.addEventListener('submit',(e)=>{
     //values inputed
     console.log(email, password)
 
-//sign up the user into the firebase auth
-auth.createUserWithEmailAndPassword(email,password).then(cred=>{
-    console.log(cred)
-    console.log(cred.user)
-    const modal = document.querySelector('#modal-signup')
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
+    //sign up the user into the firebase auth
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred)
+        const modal = document.querySelector('#modal-signup')
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    })
+
 })
 
 
-})
 //log out
-const logout =document.getElementById('logout')
-logout.addEventListener('click',(e)=>{
+logout.addEventListener('click', (e) => {
     e.preventDefault();
-auth.signOut().then(()=>{
-    console.log('user signed out')
+    auth.signOut()
 })
+
+//log in
+const login = document.querySelector('#login-form')
+login.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = login["login-email"].value
+    const password = login["login-password"].value
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred)
+        const modal = document.querySelector('#modal-login')
+        M.Modal.getInstance(modal).close();
+        login.reset();
+    })
+
 })
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,32 @@
+//access to the ul element
+const ulElem = document.querySelector('.guides')
+
+function renderGuide(data) {
+  if(data.length){
+    let html =''
+
+    data.forEach(element => {
+        console.log(element.data())
+        let guide = element.data()
+
+    const li = `
+    <li>
+    <div class="collapsible-header grey lighten-4">${guide.title}</div>
+    <div class="collapsible-body white">${guide.content}</div>
+    </li>
+    `
+    html += li
+
+    })
+
+    ulElem.innerHTML = html
+  }else{
+    ulElem.innerHTML =`<h5 class="center-align">You have logged out, <b>log in </b>to view guides</h5>`
+  }
+    
+
+}
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
